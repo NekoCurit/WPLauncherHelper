c97c9ac22f2682173a8ac5ba10d80fc04b684c14
丛雨绫喵 <nekocurit@proton.me>
2026-04-04 00:25:48 +0800

    feat: user public state (#6)
    获取用户公开状态
    点赞/取消点赞用户

--- src/main/kotlin/net/nekocurit/x19/api/Users.kt
+++ src/main/kotlin/net/nekocurit/x19/api/Users.kt
@@ -5,6 +5,7 @@ import net.nekocurit.x19.WPLauncherAccountAPI
 import net.nekocurit.x19.data.ResponseX19Base
 import net.nekocurit.x19.data.ResponseX19BaseMulti
 import net.nekocurit.x19.data.user.X19User
+import net.nekocurit.x19.data.user.X19UserPublicState
 
 suspend fun WPLauncherAccountAPI.getUser(id: ULong) = postWithAuth(
     path = "/user/query/search-by-uid",
@@ -36,13 +37,40 @@ suspend fun WPLauncherAccountAPI.searchFriends(input: String, isMail: Boolean) =
 /**
  * 主动添加好友
  *
- * @param id 用户id
+ * @param id 用户Id
  * @param message 消息
  * @param selfNick 自身名称 通过`getSelfDetail`获取
  */
 suspend fun WPLauncherAccountAPI.sendFriendRequest(id: ULong, message: String = "", selfNick: String = cacheName) = postWithAuth(
     path = "/user-apply-friend",
     body = """{"fid":$id,"comment":"$selfNick","message":"$message"}"""
+)
+    .body<ResponseX19Base>()
+    .throwOnNotOk()
+
+/**
+ * 获取用户状态
+ *
+ * @param id 用户Id
+ */
+suspend fun WPLauncherAccountAPI.getUserPublicState(id: ULong) = postWithAuth(
+    path = "/user-stat/get-user-state",
+    body = """{"search_id":"$id"}"""
+)
+    .body<ResponseX19Base>()
+    .throwOnNotOk()
+    .decode<X19UserPublicState>(this)
+
+/**
+ * 为其它用户点赞/取消点赞
+ * 该端点具有调用频率限制
+ *
+ * @param id 用户Id
+ * @param state true=点赞, false=取消
+ */
+suspend fun WPLauncherAccountAPI.likeUser(id: ULong, state: Boolean = true) = postWithAuth(
+    path = "/user-personal-page-like/update",
+    body = """{"entity_id":"${entity.entityId}","visitor_user_id":"${entity.entityId}","personal_page_owner_user_id":"$id","has_like":$state}"""
 )
     .body<ResponseX19Base>()
     .throwOnNotOk()
