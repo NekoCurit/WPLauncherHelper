7c1cf0c295ab44d31c0e0a4cc15e98907360f536
丛雨绫喵 <nekocurit@proton.me>
2026-08-04 22:54:18 +0800

    step 1

--- src/commonMain/kotlin/net/nekocurit/i4399/game_sdk/entity/I4399GameSDKOauthSession.kt
+++ src/commonMain/kotlin/net/nekocurit/i4399/game_sdk/entity/I4399GameSDKOauthSession.kt
@@ -1,36 +1,19 @@
 package net.nekocurit.i4399.game_sdk.entity
 
-import io.ktor.http.ParametersBuilder
+import io.ktor.client.statement.HttpResponse
 import kotlinx.serialization.Serializable
+import net.nekocurit.i4399.game_sdk.I4399GameSDKAPI
+import net.nekocurit.i4399.game_sdk.utils.I4399GameSDKForm
+import net.nekocurit.i4399.game_sdk.utils.decodeForms
 
 @Serializable
 data class I4399GameSDKOauthSession(
-    val clientId: String,
-    val ref: String,
-    val bizId: String,
-    val state: String,
-    val redirectUrl: String
+    val forms: I4399GameSDKForm,
 ) {
     companion object {
-        val CLIENT_ID = Regex("""<input type="hidden" name="client_id" value="(.*?)"/>""")
-        val REF = Regex("""<input type="hidden" name="ref" value="(.*?)"/>""")
-        val BIZ_ID = Regex("""<input type="hidden" name="bizId" value="(.*?)"/>""")
-        val STATE = Regex("""<input type="hidden" name="state" value="(.*?)"/>""")
-        val REDIRECT_URL = Regex("""<input type="hidden" name="redirect_uri" value="(.*?)"/>""")
-
-        fun ParametersBuilder.appendSession(session: I4399GameSDKOauthSession) = apply {
-            append("client_id", session.clientId)
-            append("ref", session.ref)
-            append("bizId", session.bizId)
-            append("state", session.state)
-            append("redirect_uri", session.redirectUrl)
-        }
+        val I4399GameSDKAPI.sessionNotNull
+            get() = requireNotNull(session) { "session 尚未初始化"}
+        suspend fun fromOauthRespond(respond: HttpResponse) = I4399GameSDKOauthSession(respond.decodeForms())
+        fun fromRawHtml(raw: String) = I4399GameSDKOauthSession(raw.decodeForms())
     }
-    constructor(raw: String): this(
-        clientId = CLIENT_ID.find(raw)?.groupValues[1]!!,
-        ref = REF.find(raw)?.groupValues[1]!!,
-        bizId = BIZ_ID.find(raw)?.groupValues[1]!!,
-        state = STATE.find(raw)?.groupValues[1]!!,
-        redirectUrl = REDIRECT_URL.find(raw)?.groupValues[1]!!,
-    )
 }
