4924d912e1ad091be77ffe4afb0aa81197998280
NekoCurit <nekocurit@proton.me>
2026-08-07 23:09:04 +0800

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,14 +25,9 @@ kotlin {
             api(libs.korlibs.crypto)
 
             api(libs.ktor.client.core)
+            api(libs.ktor.client.cio)
             api(libs.ktor.client.content.negotiation)
             api(libs.ktor.serialization.kotlinx.json)
-        }
-        jvmMain.dependencies {
-            api(libs.ktor.client.java)
-        }
-        nativeMain.dependencies {
-            api(libs.ktor.client.cio)
         }
     }
 }
